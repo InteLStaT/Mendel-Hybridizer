0dafce26f9cdb5783e73bb99dd08c5c2203a3c4c
InteLStaT <InteLStaT@users.noreply.github.com>
2017-10-30 07:08:37 +0200

    Started Genotype

--- Mendel-Hybridizer/src/org/ucoz/intelstat/mh/genetics/Genotype.java
+++ Mendel-Hybridizer/src/org/ucoz/intelstat/mh/genetics/Genotype.java
@@ -1,5 +1,18 @@
 package org.ucoz.intelstat.mh.genetics;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Genotype {
 
+	private TreeSet<AllelePair> pairs;
+	
+	public Genotype(AllelePair[] pairarray) {
+		pairs = new TreeSet();
+	}
+	
+	public Genotype(Set<AllelePair> pairset) {
+	}
+	
+	
 }
