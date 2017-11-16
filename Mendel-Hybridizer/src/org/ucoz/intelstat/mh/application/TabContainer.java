f8196647eaf35cf9151adb49fcddc718f5c804b2
InteLStaT <InteLStaT@users.noreply.github.com>
2017-11-16 15:21:03 +0200

    Removed an unnecessary line
    The tab is selected automatically when it's added. But that might change
    in the future.

--- Mendel-Hybridizer/src/org/ucoz/intelstat/mh/application/TabContainer.java
+++ Mendel-Hybridizer/src/org/ucoz/intelstat/mh/application/TabContainer.java
@@ -5,7 +5,6 @@ public class TabContainer {
 	public void actionAddTab() {
 		if (Main.tabPane() != null) {
 			Main.addNewHybridizerTab();
-			Main.tabPane().getSelectionModel().select(Main.tabPane().getTabs().size() - 2);
 		}
 	}
 
