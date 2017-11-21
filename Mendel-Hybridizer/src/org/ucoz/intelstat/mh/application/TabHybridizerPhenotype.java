a6f5515aef4f14e78113f4c0a76415e71b7f9b4f
diak <diak@GEP6.INFOLAB.NMG>
2017-11-21 13:13:06 +0200

    Properly integrated phenotype pane

---
+++ Mendel-Hybridizer/src/org/ucoz/intelstat/mh/application/TabHybridizerPhenotype.java
@@ -0,0 +1,33 @@
+package org.ucoz.intelstat.mh.application;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.apache.commons.math3.fraction.Fraction;
+import org.ucoz.intelstat.mh.genetics.Genotype;
+import org.ucoz.intelstat.mh.genetics.Phenotype;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class TabHybridizerPhenotype {
+
+	public Map<Phenotype, Fraction> ratios;
+
+	public TableView<Phenotype> tblPhenotype;
+	public TableColumn<Phenotype, String> colPt;
+	public TableColumn<Phenotype, String> colRatios;
+
+	public void initialize() {
+		// ratios is null, implement it in FilialGeneration
+		tblPhenotype.setItems(FXCollections.observableList(new ArrayList<>(ratios.keySet())));
+		colPt.setCellValueFactory((p) -> new SimpleStringProperty(p.getValue().letterRepresentation()));
+		colRatios.setCellValueFactory((p) -> new SimpleStringProperty(ratios.get(p.getValue()).toString()));
+	}
+
+	public TabHybridizerPhenotype(Map<Phenotype, Fraction> ratios) {
+		this.ratios = ratios;
+	}
+}
